c4972ee273032a68f9f8a11b7eaeb485b64f2798
Görkem <a.gorkemunuvar@gmail.com>
2023-01-10 22:57:09 +0100

    feat: (checkAlerts) add getFcmToken helper function

--- functions/src/functions/check_alerts.ts
+++ functions/src/functions/check_alerts.ts
@@ -2,14 +2,22 @@ import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import axios from 'axios';
 
+interface StringObject {
+    [key: string]: string
+}
+
+let tokensCache: StringObject = {};
+
 const checkAlertsImpl = async (context: functions.EventContext) => {
     functions.logger.log('Function is working...');
+    functions.logger.log('Tokens cache');
+    functions.logger.log(tokensCache);
 
-    const alertsRef = await admin.firestore().collection('alerts');
+    const alertsRef = admin.firestore().collection('alerts');
     const alertsSnapshot = await alertsRef.where('isActive', '==', true).get();
 
     const coinIds = await getSavedCoinIds();
-    const currentPrices = await fetchCoinPrices(coinIds);
+    const currentPrices = await fetchCurrentCoinPrices(coinIds);
 
     for (const alert of alertsSnapshot.docs) {
         const { coinId, price, type, userId } = alert.data();
@@ -17,14 +25,20 @@ const checkAlertsImpl = async (context: functions.EventContext) => {
 
         if (!currentPrice) continue;
 
-        const isPriceAbove = type === 'above' && currentPrice >= price;
-        const isPriceBelow = type === 'below' && currentPrice <= price;
+        const isPriceAboveTarget = type === 'above' && currentPrice >= price;
+        const isPriceBelowTarget = type === 'below' && currentPrice <= price;
 
-        if (isPriceAbove || isPriceBelow) {
+        if (isPriceAboveTarget || isPriceBelowTarget) {
             const message = `Coin ${coinId} has reached the target price of ${price}!`;
-            functions.logger.log(message);
-            await sendPushNotification(userId, message);
 
+            const token = await getFcmTokenByUserId(userId);
+            sendPushNotification(token, message);
+
+            const isTokenCached = userId in tokensCache;
+            if (!isTokenCached) tokensCache[userId] = token;
+
+            functions.logger.log(message);
+            //TODO: After sending the notifications set isActive to false.
         }
     }
 };
@@ -38,7 +52,7 @@ const getSavedCoinIds = async (): Promise<Array<string>> => {
 const baseUrl = 'https://api.coingecko.com/api/v3/simple/price?';
 const currencies = 'usd';
 
-const fetchCoinPrices = async (coinIds: Array<string>) => {
+const fetchCurrentCoinPrices = async (coinIds: Array<string>) => {
     const formattedCoinIds = coinIds.join(',');
     const url = `${baseUrl}&vs_currencies=${currencies}&ids=${formattedCoinIds}`;
     const response = await axios.get(url);
@@ -46,9 +60,27 @@ const fetchCoinPrices = async (coinIds: Array<string>) => {
     return response.status == 200 ? response.data : {};
 }
 
+const getFcmTokenByUserId = async (id: string): Promise<string> => {
+    const isTokenCached = id in tokensCache;
+    if (isTokenCached) {
+        return tokensCache[id];
+    }
 
-function sendPushNotification(userId: string, message: string): Promise<void> {
-    // Send a push notification to the user using a service such as Firebase Cloud Messaging or Apple Push Notification Service
+    return getFcmTokenFromFirestore(id);
+}
+
+const getFcmTokenFromFirestore = async (userId: string): Promise<string> => {
+    const usersRef = admin.firestore().collection('users').doc(userId);
+    const userSnapshot = await usersRef.get();
+    const data = userSnapshot.data();
+
+    return data?.fcmToken;
+}
+
+function sendPushNotification(fcmToken: string, message: string): Promise<void> {
+    if (!fcmToken) return Promise.resolve();
+
+    functions.logger.log(`Sending alert to this token = ${fcmToken}`);
     return Promise.resolve();
 }
 
